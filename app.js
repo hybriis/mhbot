50615539136f2dd59e7e42c6ebcd42d9a8096b8b
Lance Hymel <hybriiss@outlook.com>
2018-01-16 13:11:23 -0600

    nothing

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ fs.readdir('./commands/', (err, files) => {
   log(`Loading a total of ${files.length} commands.`);
   files.forEach(f => {
     let props = require(`./commands/${f}`);
-    log(`Loading Command: ${props.help.name}. 👌`);
+    log(`Loading Command: ${props.help.name}.`);
     client.commands.set(props.help.name, props);
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
