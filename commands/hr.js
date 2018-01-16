8673a0568243a3bdaf5d55968b020a782231e536
Lance Hymel <hybriiss@outlook.com>
2018-01-16 14:04:52 -0600

    Moved hr roles list to settings.json and updated hr command accordingly

--- commands/hr.js
+++ commands/hr.js
@@ -1,34 +1,42 @@
+const Discord = require('discord.js');
+const settings = require('../settings.json');
+
 exports.run = (client, message, args) => {
   let hr = Math.floor(args[0]);
   if (isNaN(hr) || hr < 1 || hr > 999) {
     message.channel.send('HR must be a number 1-999.');
   } else {
     let roles = message.guild.roles;
-    // list of all hrRoles on the server.
-    // Use settings.json later to store list of HR roles and pick the correct one generically.
-    let hrRoles = [roles.find('name','HR 1+'), roles.find('name','HR 4+'), roles.find('name','HR 7+'), roles.find('name','HR 100+')];
-    let newRole;
+
+    // create collection of all hr roles from settings.json
+    let hrRoles = new Discord.Collection();
+    settings.hrroles.forEach( function (int) {
+      hrRoles.set(int, roles.find('name','HR ' + int + '+'));
+    });
 
     // set role that matches the HR provided
-    if (hr < 4)
-      newRole = roles.find('name','HR 1+');
-    else if (hr < 7)
-      newRole = roles.find('name','HR 4+');
-    else if (hr < 100)
-      newRole = roles.find('name','HR 7+');
-    else
-      newRole = roles.find('name','HR 100+');
+    let newRole;
+    hrRoles.keyArray().reverse().find( function (int) {
+      if (hr >= int) {
+        newRole = hrRoles.get(int);
+        return true;
+      }
+    });
 
     // add matching HR role if it's not already there
     if (!message.member.roles.exists('name', newRole.name))
       message.member.addRole(newRole);
 
     // remove any conflicting roles
-    let filteredRoles = hrRoles.filter(function (role) { return role.name !== newRole.name; });
+    let filteredRoles = hrRoles.filter(function (role) {
+      return role.name !== newRole.name;
+    });
     filteredRoles.forEach( function(role) {
       if (message.member.roles.exists('name', role.name))
         message.member.removeRole(role);
     });
+
+    message.channel.send('HR set to ' + hr + '.');
 
     // TODO: setup persistant storage (mongodb?) to store the exact HR
   }
