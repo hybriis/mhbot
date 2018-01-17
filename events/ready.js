aec52b423023c785b3eb7c2fae982a6f14dbebc0
Lance Hymel <hybriiss@outlook.com>
2018-01-17 09:16:42 -0600

    set bot game on ready

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,6 @@
 const chalk = require('chalk');
+const settings = require('../settings.json');
 module.exports = client => {
+  client.user.setPresence( {game: { name: settings.prefix + 'help' } } );
   console.log(chalk.bgGreen.black('I\'m Online'));
 };
