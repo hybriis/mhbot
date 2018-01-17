960ceda0b6263e5704b9ff67dbf533d3188f30a0
Lance Hymel <hybriiss@outlook.com>
2018-01-17 12:15:37 -0600

    hr command now uses existing HR roles on server instead of settings.json

--- commands/hr.js
+++ commands/hr.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const settings = require('../settings.json');
+//const settings = require('../settings.json');
 
 exports.run = (client, message, args) => {
   let hr = Math.floor(args[0]);
@@ -8,35 +8,46 @@ exports.run = (client, message, args) => {
   } else {
     let roles = message.guild.roles;
 
-    // create collection of all hr roles from settings.json
+    // create collection of all HR roles from the guild's role list
     let hrRoles = new Discord.Collection();
-    settings.hrroles.forEach( function (int) {
-      hrRoles.set(int, roles.find('name','HR ' + int + '+'));
+    var pattern = /HR\s(\d+)\+/;
+    roles.forEach( function (role) {
+      let match = role.name.match(pattern);
+      if (match != null) {
+        hrRoles.set(Math.floor(match[1]), role);
+      }
+    });
+
+    // reverse sort the map by key (HR number)
+    hrRoles = hrRoles.sort( function (a,b) {
+      return Math.floor(b.name.match(pattern)[1]) - Math.floor(a.name.match(pattern)[1]);
     });
 
     // set role that matches the HR provided
     let newRole;
-    hrRoles.keyArray().reverse().find( function (int) {
+    hrRoles.keyArray().find( function (int) {
       if (hr >= int) {
         newRole = hrRoles.get(int);
         return true;
       }
     });
 
-    // add matching HR role if it's not already there
-    if (!message.member.roles.exists('name', newRole.name))
-      message.member.addRole(newRole);
+    if (newRole != null) {
+      // add matching HR role if it's not already there
+      if (!message.member.roles.exists('name', newRole.name))
+        message.member.addRole(newRole);
 
-    // remove any conflicting roles
-    let filteredRoles = hrRoles.filter(function (role) {
-      return role.name !== newRole.name;
-    });
-    filteredRoles.forEach( function(role) {
-      if (message.member.roles.exists('name', role.name))
-        message.member.removeRole(role);
-    });
+      // remove any conflicting roles
+      let filteredRoles = hrRoles.filter(function (role) {
+        return role.name !== newRole.name;
+      });
+      filteredRoles.forEach( function(role) {
+        if (message.member.roles.exists('name', role.name))
+          message.member.removeRole(role);
+      });
 
-    message.channel.send('HR set to ' + hr + '.');
+      message.channel.send('HR set to ' + hr + '.');
+    } else message.channel.send('No matching HR role found.');
 
     // TODO: setup persistant storage (mongodb?) to store the exact HR
   }
