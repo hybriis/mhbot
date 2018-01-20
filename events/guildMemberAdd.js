83f98fc4a3b21f7dc8e60699fea7eebf951cb875
Lance Hymel <hybriiss@outlook.com>
2018-01-20 11:24:04 -0600

    error handle the new member hr setting

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,4 @@
 module.exports = member => {
-  member.addRole(member.guild.roles.find('name','HR 1+'));
+  member.addRole(member.guild.roles.find('name','HR 1+'))
+    .catch((e) => console.log(`HR 1+ role missing.\n${e}`));
 };
